20b9e01c3f50ebe2c42ca77efca19b3a3d179b0e
DerryChan <derrychan@foxmail.com>
2016-11-22 23:08:09 +0800

    cyh

--- app/src/main/java/com/communistpartyconstruction/Activity/MyPointActivity.java
+++ app/src/main/java/com/communistpartyconstruction/Activity/MyPointActivity.java
@@ -2,16 +2,45 @@ package com.communistpartyconstruction.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.communistpartyconstruction.R;
 
 public class MyPointActivity extends AppCompatActivity {
-
+    private TextView name,totalPoint,growth,done0,done1,done2,done3,done4;
+    private ImageView hook0,hook1,hook2,hook3,hook4;
+    private Button goBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_point);
-        TextView
+        initUI();
+
     }
+    private void initUI(){
+        name = (TextView) findViewById(R.id.myPoint_name);
+        totalPoint = (TextView) findViewById(R.id.myPoint_totalPoint);
+        growth = (TextView) findViewById(R.id.myPoint_growth);
+        done0 = (TextView) findViewById(R.id.myPoint_done0);
+        done1 = (TextView) findViewById(R.id.myPoint_done1);
+        done2 = (TextView) findViewById(R.id.myPoint_done2);
+        done3 = (TextView) findViewById(R.id.myPoint_done3);
+        done4 = (TextView) findViewById(R.id.myPoint_done4);
+        hook0 = (ImageView) findViewById(R.id.myPoint_hook0);
+        hook1 = (ImageView) findViewById(R.id.myPoint_hook1);
+        hook2 = (ImageView) findViewById(R.id.myPoint_hook2);
+        hook3 = (ImageView) findViewById(R.id.myPoint_hook3);
+        hook4 = (ImageView) findViewById(R.id.myPoint_hook4);
+        goBack = (Button) findViewById(R.id.myPoint_goBack);
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/communistpartyconstruction/Fragment/MeFragment.java
+++ app/src/main/java/com/communistpartyconstruction/Fragment/MeFragment.java
@@ -6,8 +6,12 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.communistpartyconstruction.R;
+import com.communistpartyconstruction.support.CircleImageView;
 
 /**
  * Created by hekesong on 2016/11/15.
@@ -15,18 +19,57 @@ import com.communistpartyconstruction.R;
 
 public class MeFragment extends Fragment implements View.OnClickListener {
     private View view;
+    private RelativeLayout integral,history,share,message,suggest,setting;
+    private CircleImageView headView;
+    private TextView myName,mySchool;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         if (view == null){
             view = inflater.inflate(R.layout.me_fragment,container,false);
         }
-
+        initUI();
         return  view;
     }
+    private void initUI(){
+        integral = (RelativeLayout) view.findViewById(R.id.meFragment_integral);
+        history = (RelativeLayout) view.findViewById(R.id.meFragment_history);
+        share = (RelativeLayout) view.findViewById(R.id.meFragment_share);
+        message = (RelativeLayout) view.findViewById(R.id.meFragment_message);
+        suggest = (RelativeLayout) view.findViewById(R.id.meFragment_suggest);
+        setting = (RelativeLayout) view.findViewById(R.id.meFragment_setting);
+        headView = (CircleImageView) view.findViewById(R.id.meFragment_headView);
+        myName = (TextView) view.findViewById(R.id.meFragment_name);
+        mySchool = (TextView) view.findViewById(R.id.meFragment_school);
 
+        integral.setOnClickListener(this);
+        history.setOnClickListener(this);
+        share.setOnClickListener(this);
+        message.setOnClickListener(this);
+        suggest.setOnClickListener(this);
+        setting.setOnClickListener(this);
+    }
     @Override
     public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.meFragment_integral:
+                break;
+            case R.id.meFragment_history:
+                break;
+            case R.id.meFragment_share:
+                break;
+            case R.id.meFragment_message:
+                break;
+            case R.id.meFragment_suggest:
+                break;
+            case R.id.meFragment_setting:
+                break;
+            case R.id.meFragment_headView:
+                break;
 
+            default:
+                break;
+        }
     }
 }
