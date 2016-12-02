4a7ce70c7379c66256a1217e649c03d493d0b98d
DerryChan <derrychan@foxmail.com>
2016-12-02 23:21:58 +0800

    cyh

--- app/src/main/java/com/communistpartyconstruction/Adapter/InteractiveRecycleViewAdapter.java
+++ app/src/main/java/com/communistpartyconstruction/Adapter/InteractiveRecycleViewAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.communistpartyconstruction.R;
@@ -15,20 +16,20 @@ import com.communistpartyconstruction.R;
 
 public class InteractiveRecycleViewAdapter extends RecyclerView.Adapter<InteractiveRecycleViewAdapter.ViewHolder> {
     private LayoutInflater mInflater;
-    private String[] mTitles=null;
+    private String[] mTitles = null;
 
-    public InteractiveRecycleViewAdapter(Context context){
-        this.mInflater=LayoutInflater.from(context);
-        this.mTitles=new String[20];
-        for (int i=0;i<20;i++){
-            int index=i+1;
-            mTitles[i]="item"+index;
+    public InteractiveRecycleViewAdapter(Context context) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mTitles = new String[20];
+        for (int i = 0; i < 20; i++) {
+            int index = i + 1;
+            mTitles[i] = "item" + index;
         }
     }
+
     @Override
-    public ViewHolder  onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.interactive_list_item,parent,false);
-        //view.setBackgroundColor(Color.RED);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.interactive_list_item, parent, false);
         ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
@@ -36,6 +37,13 @@ public class InteractiveRecycleViewAdapter extends RecyclerView.Adapter<Interact
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.item_tv.setText(mTitles[position]);
+        holder.item_title.setText(mTitles[position]);
+        holder.body.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 
@@ -45,12 +53,14 @@ public class InteractiveRecycleViewAdapter extends RecyclerView.Adapter<Interact
     }
 
     //自定义的ViewHolder，持有每个Item的的所有界面元素
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView item_tv;
-
-        public ViewHolder(View view) {
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView item_tv, item_title;
+        RelativeLayout body;
+        ViewHolder(View view) {
             super(view);
             item_tv = (TextView) view.findViewById(R.id.interactive_list_text);
+            item_title = (TextView) view.findViewById(R.id.interactive_list_title);
+            body = (RelativeLayout) view.findViewById(R.id.interactive_list_body);
         }
     }
 }
--- app/src/main/java/com/communistpartyconstruction/Fragment/InteractiveFragment.java
+++ app/src/main/java/com/communistpartyconstruction/Fragment/InteractiveFragment.java
@@ -3,12 +3,16 @@ package com.communistpartyconstruction.Fragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
+import com.communistpartyconstruction.Adapter.Decoration.InteractiveRecycleViewDecoration;
+import com.communistpartyconstruction.Adapter.InteractiveRecycleViewAdapter;
 import com.communistpartyconstruction.R;
 
 /**
@@ -19,6 +23,8 @@ public class InteractiveFragment extends Fragment implements View.OnClickListene
     private View view;
     private RelativeLayout infoCheck,submitApp,rules,questionnaire,fileInfo,videoInfo;
     private RecyclerView memberList;
+    private RecyclerView.Adapter mAdapter;
+    private LinearLayoutManager mLayoutManager;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -37,7 +43,19 @@ public class InteractiveFragment extends Fragment implements View.OnClickListene
         fileInfo = (RelativeLayout) view.findViewById(R.id.interactive_fileInfo);
         videoInfo = (RelativeLayout) view.findViewById(R.id.interactive_videoInfo);
         memberList = (RecyclerView) view.findViewById(R.id.interactive_list);
-
+        //设置固定大小
+        memberList.setHasFixedSize(true);
+        //创建线性布局
+        mLayoutManager = new LinearLayoutManager(this.getActivity());
+        //垂直方向
+        mLayoutManager.setOrientation(OrientationHelper.VERTICAL);
+        //给RecyclerView设置布局管理器
+        memberList.setLayoutManager(mLayoutManager);
+        //创建适配器，并且设置
+        mAdapter = new InteractiveRecycleViewAdapter(this.getActivity());
+        memberList.setAdapter(mAdapter);
+        //添加分隔线
+        memberList.addItemDecoration(new InteractiveRecycleViewDecoration(this.getActivity(),OrientationHelper.VERTICAL));
     }
 
     @Override
