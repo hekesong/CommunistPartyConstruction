330b3b2c16f9b681a1714df538949e3bfabf98cc
何科松 <930462289@qq.com>
2016-12-14 14:20:33 +0800

    解决scrollview和recycleview的冲突

--- app/src/main/java/com/communistpartyconstruction/Fragment/HomeFragment.java
+++ app/src/main/java/com/communistpartyconstruction/Fragment/HomeFragment.java
@@ -3,16 +3,17 @@ package com.communistpartyconstruction.Fragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 import com.communistpartyconstruction.Adapter.Decoration.InteractiveRecycleViewDecoration;
 import com.communistpartyconstruction.Adapter.PartyBuildingNewsadAdapter;
 import com.communistpartyconstruction.R;
+import com.communistpartyconstruction.Support.FullyLinearLayoutManager;
 
 /**
  * Created by hekesong on 2016/11/15.
@@ -21,8 +22,9 @@ import com.communistpartyconstruction.R;
 public class HomeFragment extends Fragment{
     private View view;
     private RecyclerView recycleView;
-    private LinearLayoutManager mLayoutManager;
+    private FullyLinearLayoutManager fullyLinearLayoutManager;
     private PartyBuildingNewsadAdapter adapter;
+    private ScrollView scrollView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -36,11 +38,21 @@ public class HomeFragment extends Fragment{
     private void initView() {
         recycleView = (RecyclerView) view.findViewById(R.id.home_list);
         recycleView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this.getActivity());
-        mLayoutManager.setOrientation(OrientationHelper.VERTICAL);
-        recycleView.setLayoutManager(mLayoutManager);
+        recycleView.setNestedScrollingEnabled(false);
+        fullyLinearLayoutManager = new FullyLinearLayoutManager(getActivity()){
+            @Override
+            public boolean canScrollVertically() {
+                return false;
+            }
+        };
+
+        fullyLinearLayoutManager.setOrientation(OrientationHelper.VERTICAL);
+        recycleView.setLayoutManager(fullyLinearLayoutManager);
         adapter = new PartyBuildingNewsadAdapter(this.getActivity());
         recycleView.setAdapter(adapter);
         recycleView.addItemDecoration(new InteractiveRecycleViewDecoration(this.getActivity(),OrientationHelper.VERTICAL));
+        //解决打开APP后，直接滑到recycleView的位置，上面的view被遮挡
+        scrollView = (ScrollView) view.findViewById(R.id.home_fragment_scrollview);
+        scrollView.smoothScrollTo(0,20);
     }
 }
